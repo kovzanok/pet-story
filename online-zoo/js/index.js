303670054a6ccc9a1ae9c99c1c514bb10179c442
kovzanok <alexkovzanok@gmail.com>
2023-02-12 16:19:48 +0300

    feat: add modal from 860px testimonials

--- online-zoo/js/index.js
+++ online-zoo/js/index.js
@@ -1,35 +1,117 @@
-const menuButton=document.querySelector('.hamburger');
-const menu=document.querySelector('.menu__wrapper')
+const menuButton = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu__wrapper");
 
-const closeMenu=()=>{
-    menu.classList.remove('menu__wrapper_active');
-    menuButton.classList.remove('hamburger_active');
-    document.body.classList.remove('body_lock');
+const closeMenu = () => {
+  menu.classList.remove("menu__wrapper_active");
+  menuButton.classList.remove("hamburger_active");
+  document.body.classList.remove("body_lock");
+};
+
+const openMenu = () => {
+  menu.classList.add("menu__wrapper_active");
+  menuButton.classList.add("hamburger_active");
+  document.body.classList.add("body_lock");
+};
+
+const menuClickHandler = function (e) {
+  if (e.target.classList.contains("menu__wrapper")) {
+    closeMenu();
+  }
+};
+
+const menuButtonClickHandler = function (e) {
+  if (this.classList.contains("hamburger_active")) {
+    closeMenu();
+  } else {
+    openMenu();
+    menu.addEventListener("click", menuClickHandler);
+  }
+};
+
+const testimonialsClickHandler = (e) => {
+  if (e.target.closest(".testimonial__wrapper")) {
+    const testimonial = e.target.closest(".testimonial__wrapper");
+    const testimonialInfo = getTestimonialInfo(testimonial);
+    const modal = generateModal(testimonialInfo);
+    addTestimonialProfilePic(modal,testimonialInfo.pic);
+    addModalToDocument(modal);
+    modal.addEventListener("click", modalClickHandler);
+  }
+};
+
+const closeModal = (modal) => {
+  document.body.classList.remove("body_lock");
+  modal.remove();
+};
+
+const getTestimonialInfo = (testimonial) => {
+  const info = {
+    name: testimonial.querySelector(".person-name").textContent,
+    pic: getComputedStyle(testimonial.querySelector(".profile__pic"))
+      .backgroundImage,
+    location: testimonial.querySelector(".person-location").textContent,
+    date: testimonial.querySelector(".person-date").textContent,
+    comment: testimonial.querySelector(".comment__paragraph").textContent,
+  };
+
+  return info;
+};
+
+const generateModal = (info) => {
+  const modal = document.createElement("DIV");
+
+  modal.classList.add("modal__wrapper");
+  modal.innerHTML = `<div class="modal__body">
+    <div class="modal__close-button"></div>
+    <div class="modal__content">
+      <div class="testimonial__wrapper">
+        <div class="testimonial">
+          <div class="testimonial__profile">
+            <div class="profile__pic"></div>
+            <div class="profile__info">
+              <h5 class="person-name">${info.name}</h5>
+              <div class="person-info">
+                <span class="person-location">${info.location}</span>
+                <span>•</span>
+                <span class="person-date">${info.date}</span>
+              </div>
+            </div>
+          </div>
+          <div class="testimonial__comment">
+            <p class="comment__paragraph">
+            ${info.comment}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+  return modal;
+};
+
+const addModalToDocument = (modal) => {
+  document.querySelector("footer").after(modal);
+  document.body.classList.add("body_lock");
+};
+
+const addTestimonialProfilePic = (modal, picUrl) => {
+  modal.querySelector(".profile__pic").style.backgroundImage = picUrl;
+};
+
+const modalClickHandler = function (e) {
+  
+  if (
+    e.target.classList.contains("modal__wrapper") ||
+    e.target.classList.contains("modal__close-button")||
+    e.target.classList.contains("modal__body")
+  ) {
+    closeModal(this);
+  }
+};
+
+menuButton.addEventListener("click", menuButtonClickHandler);
+if (window.innerWidth <= 860) {
+  const testimonialsList = document.querySelector(".layout-4-columns");
+  testimonialsList.addEventListener("click", testimonialsClickHandler);
 }
-
-const openMenu=()=>{
-    menu.classList.add('menu__wrapper_active');
-    menuButton.classList.add('hamburger_active');
-    document.body.classList.add('body_lock');
-}
-
-const menuClickHandler=function(e){
-    if (e.target.classList.contains('menu__wrapper')){
-        closeMenu();
-    }
-}
-
-
-const menuButtonClickHandler=function (e){
-    
-    if (this.classList.contains('hamburger_active')){
-        closeMenu();
-    }
-    else{
-        openMenu();
-        menu.addEventListener('click', menuClickHandler)
-    }
-}
-
-
-menuButton.addEventListener('click', menuButtonClickHandler)
